9ce1d5bc7c90f68e8390e82076d01ae75d585570
rbagchi0715 <82537335+rbagchi0715@users.noreply.github.com>
2022-12-10 17:59:33 -0800

    Additional functionality added

--- Darkness/Assets/Scripts/StartButton.cs
+++ Darkness/Assets/Scripts/StartButton.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
-    public GameObject LoadGame;
-    // Start is called before the first frame update
+    public GameObject LoadGame; //Panel for player to select their new or saved game files.
+    public GameObject Close_LoadGame; //Button to close the current panel.
 
+    //Function to open panel when "Start" button is pressed.
     public void OpenLoadGame()
     {
+        //Function to display panel and exit button
         if(LoadGame != null)
         {
             LoadGame.SetActive(true);
+        }
+
+        if (Close_LoadGame != null)
+        {
+            Close_LoadGame.SetActive(true);
         }
     }
 }
