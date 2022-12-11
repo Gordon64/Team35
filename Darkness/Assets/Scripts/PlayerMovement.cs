3786a8cc459353e37b538938142a2cfa95345693
Sharyn <sharyns@nevada.unr.edu>
2022-12-11 04:36:53 -0800

    update

--- Darkness/Assets/Scripts/PlayerMovement.cs
+++ Darkness/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
@@ -10,13 +11,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
+
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal");     //moving left to right
+        float moveVertical = Input.GetAxis("Vertical");
 
         Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
 
         float newVelocityX = 0f;
+        float newVelocityY = 0f;    
 
         if(moveHorizontal < 0 && currentVelocity.x <= 0)
         {
@@ -31,6 +35,11 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             //idle animation
+        }
+
+        if(moveVertical > 0 && currentVelocity.y >= 0)
+        {
+            newVelocityY = speed;
         }
 
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newVelocityX, 0);
