60e27661c9e52f7f2b7da87d718163ecb1380763
Dieu_John <70336903+DieuJohn@users.noreply.github.com>
2022-12-10 16:00:53 -0800

    Baseline for Playermovement and Animation, Basic Level Scene and Background

---
+++ Darkness/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Animator animator;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+
+        float newVelocityX = 0f;
+
+        if(moveHorizontal < 0 && currentVelocity.x <= 0)
+        {
+            newVelocityX = -speed;
+            //animator for left goes here
+        }
+        else if (moveHorizontal > 0 && currentVelocity.x >= 0)
+        {
+            newVelocityX = speed;
+            //animator for right goes here
+        }
+        else
+        {
+            //idle animation
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newVelocityX, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
