17cab7a07b1a25a142806e0964cc867abf5bebb1
rbagchi0715 <82537335+rbagchi0715@users.noreply.github.com>
2022-12-11 20:39:24 -0800

    Updated StartScene

---
+++ Darkness/Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueButton : MonoBehaviour
+{
+    public GameObject LoadGamePanel; //Panel for player to select their new or saved game files.
+    public GameObject Close_LoadGameButton; //Button to close the current panel.
+
+    //Function to open panel when "Continue" button is pressed.
+    public void OpenLoadGame()
+    {
+        //Function to display panel and exit button
+        if (LoadGamePanel != null)
+        {
+            LoadGamePanel.SetActive(true);
+        }
+
+        if (Close_LoadGameButton != null)
+        {
+            Close_LoadGameButton.SetActive(true);
+        }
+    }
+}
--- Darkness/Assets/Scripts/StartButton.cs
+++ Darkness/Assets/Scripts/StartButton.cs
@@ -1,24 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    public GameObject LoadGame; //Panel for player to select their new or saved game files.
-    public GameObject Close_LoadGame; //Button to close the current panel.
-
-    //Function to open panel when "Start" button is pressed.
-    public void OpenLoadGame()
+    public void StartLevel(string sceneName)
     {
-        //Function to display panel and exit button
-        if (LoadGame != null)
-        {
-            LoadGame.SetActive(true);
-        }
-
-        if (Close_LoadGame != null)
-        {
-            Close_LoadGame.SetActive(true);
-        }
+        SceneManager.LoadScene(sceneName);
     }
 }
