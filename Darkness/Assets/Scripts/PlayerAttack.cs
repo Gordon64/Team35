83204790bd548b683aabbc34a8e75f1120bf0b4a
Sharyn <sharyns@nevada.unr.edu>
2022-12-11 17:13:43 -0800

    attack

--- Darkness/Assets/Scripts/PlayerAttack.cs
+++ Darkness/Assets/Scripts/PlayerAttack.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float attackCooldown;
+    private Animator an;
+    private PlayerMovement pMove;
+    private float cooldown = Mathf.Infinity;
+
+    // 
+    private void Awake()
     {
-        
+        //an = GetCompopnent<Animator>();
+        //pMove = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && cooldown > attackCooldown )
+        {
+            Attack();
+        }
+
+        cooldown += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        GetComponent<Animator>().SetTrigger("attack");
+        cooldown = 0;
     }
 }
