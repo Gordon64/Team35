a655e8c7c30f3e1a776854f60c14347247c35f89
Gordon64 <89490405+Gordon64@users.noreply.github.com>
2023-02-20 01:18:02 -0800

    Added Player Defend and Run

--- Darkness/Assets/Scripts/AttackArea.cs
+++ Darkness/Assets/Scripts/AttackArea.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class AttackArea : MonoBehaviour
 {
-    private int damage = 3;
+    public int damage = 3;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.GetComponent<EnemyStats>() != null)
         {
-            EnemyStats health = collider.GetComponent<EnemyStats>();
-            health.takeDamage(damage);
+            EnemyStats enemy = collider.GetComponent<EnemyStats>();
+            enemy.takeDamage(damage);
+            //Vector2 direction = (collider.transform.position - enemy.transform.position).normalized;
+            StartCoroutine(enemy.Knockback(0.02f, 500, enemy.transform.position));
         }
     }
 }
