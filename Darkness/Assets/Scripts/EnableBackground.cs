718fd1b3787465ee75091e01f2918e8c40ce7439
Diegolamas1 <dlamas@nevada.unr.edu>
2023-03-10 09:01:41 -0800

    fixed Shop and got functionality
    Was able to get functionality to work for selecting an item. Item background now enables when an Item is selected and is disabled when another item is selected. scriptable item information in ShopItem list can now be passed over to different facets of the description. Some information needs to be added to scriptable items but can all be added pretty quickly. Last thing that needs to be added to the shop on a implementation standpoint is the action of buying and adding items, which also comes with implementing an economy system.

--- Darkness/Assets/Scripts/EnableBackground.cs
+++ Darkness/Assets/Scripts/EnableBackground.cs
@@ -2,14 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnableBackground : MonoBehaviour
 {
     public GameObject Background;
+    private Shop_Manager shopManager;
+    public GameObject thisPrefab;
+    public GameObject SelectedImagePanel;
+    public Image[] ItemSprite;
+
+    void Start(){
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Shop");
+        if(playerObject != null){
+            shopManager = playerObject.GetComponent<Shop_Manager>();
+        }
+        else{
+            UnityEngine.Debug.Log("can't find GameObject");
+        }
+        SelectedImagePanel = GameObject.FindGameObjectWithTag("SelectedItem");
+        ItemSprite = SelectedImagePanel.GetComponentsInChildren<Image>();
+    }
 
     public void Enable()
     {
+        for(int i = 0; i < shopManager.ItemPrefabs.Length; i++){
+            if(shopManager.ItemPrefabs[i] == null){
+                UnityEngine.Debug.Log("couldn't find prefabs list");
+            }
+            else{
+                if(thisPrefab == shopManager.ItemPrefabs[i]){
+                    UnityEngine.Debug.Log("prefab found");
+                    DisplayItemInfo(shopManager.ShopItems[i]);
+                }
+                else{
+                    UnityEngine.Debug.Log("no prefab found");
+                    shopManager.ItemPrefabs[i].transform.Find("Background").gameObject.SetActive(false);
+                }
+            }
+        }
         Background.SetActive(true);
+    }
+
+    void DisplayItemInfo(InventoryItem Item){
+        foreach(Image imageComponent in ItemSprite){
+            if(imageComponent.name == "ImageBoarder"){
+                imageComponent.sprite = Item.itemIcon;
+            }
+            //add name and description that need to be passed to text boxes
+        }
     }
 
     void Update()
--- Darkness/Assets/Scripts/Inventory/Shop_Manager.cs
+++ Darkness/Assets/Scripts/Inventory/Shop_Manager.cs
@@ -8,6 +8,8 @@ public class Shop_Manager : MonoBehaviour
 {
     public static Shop_Manager instance;
     public List<InventoryItem> ShopItems = new List<InventoryItem>();
+    public GameObject[] ItemPrefabs;
+    public int context = 0;
 
     public Transform ItemContent;
     public GameObject Item;
@@ -17,8 +19,14 @@ public class Shop_Manager : MonoBehaviour
         foreach (var anItem in ShopItems)
         {
             GameObject ob = Instantiate(Item, ItemContent);
+            if(context >= ItemPrefabs.Length){
+                System.Array.Resize(ref ItemPrefabs, context + 1);
+            }
+            ItemPrefabs[context] = ob;
+            
             var ItemImage = ob.transform.Find("Image").GetComponent<Image>();
 
+            context++;
             ItemImage.sprite = anItem.itemIcon;
         }
     }
