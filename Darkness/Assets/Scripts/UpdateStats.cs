2333826d26a16e03de2448e2f042d254e2fa687a
rbagchi0715 <82537335+rbagchi0715@users.noreply.github.com>
2023-04-04 18:01:20 -0700

    Optimizing Upgrade System (WIP)

--- Darkness/Assets/Scripts/UpdateStats.cs
+++ Darkness/Assets/Scripts/UpdateStats.cs
@@ -5,9 +5,23 @@ public class UpdateStats : MonoBehaviour
 {
     public TextMeshProUGUI m_text;
 
-
-    public void UpdateStat(string text)
+    public void UpdateStat()
     {
-        m_text.text = text;
+        string temp = m_text.text;
+        int value = System.Convert.ToInt32(temp);
+
+        if (value >= 30)
+        {
+            value = value + 5;
+            temp = System.Convert.ToString(value);
+            m_text.text = temp;
+        }
+
+        if (value <= 4)
+        {
+            value = value - 1;
+            temp = System.Convert.ToString(value);
+            m_text.text = temp;
+        }
     }
 }
