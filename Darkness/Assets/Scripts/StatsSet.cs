45a4e4082c10ccd380f476a3a0363b2243c937e8
rbagchi0715 <82537335+rbagchi0715@users.noreply.github.com>
2023-04-15 21:42:43 -0700

    Stats Panel now uses actual player stats rather than placeholder values.

---
+++ Darkness/Assets/Scripts/StatsSet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class StatsSet : MonoBehaviour
+{
+    public TextMeshProUGUI healthStat;
+    public TextMeshProUGUI attackStat;
+    public TextMeshProUGUI defenseStat;
+    public TextMeshProUGUI energyStat;
+
+    public GameObject StatsPanel;
+
+    public UnitStats Player;
+
+    public void SetStats()
+    {
+        float playerHealth = Player.health;
+        float playerAttack = Player.attack;
+        float playerDefense = Player.defense;
+        float playerEnergy = Player.energy;
+
+        healthStat.text = System.Convert.ToString(playerHealth);
+        attackStat.text = System.Convert.ToString(Player.attack);
+        defenseStat.text = System.Convert.ToString(Player.defense);
+        energyStat.text = System.Convert.ToString(Player.energy);
+    }
+
+    void Start()
+    {
+        SetStats();
+    }
+}
--- Darkness/Assets/Scripts/UpdateStats.cs
+++ Darkness/Assets/Scripts/UpdateStats.cs
@@ -29,7 +29,6 @@ public class UpdateStats : MonoBehaviour
             {
                 Bandit.health = Bandit.health + 5;
             }
-            
         }
         
     }
