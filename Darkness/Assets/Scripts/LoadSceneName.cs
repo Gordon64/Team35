d75c46b898b3b0c4113866aa637122f1bd9fca7d
Gordon64 <89490405+Gordon64@users.noreply.github.com>
2023-04-24 03:30:06 -0700

    Added Scene & Level Transitions
    Added invisible walls for now to activate scene transitions in the levels. The cutscenes will automatically transition to the next level when finished.

---
+++ Darkness/Assets/Scripts/LoadSceneName.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneName : MonoBehaviour
+{
+    public string sceneName = "";
+    /*
+    void OnEnable()
+    {
+        SceneManager.LoadScene("TransitionLevel1", LoadSceneMode.Single);
+    }
+    */
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+
+    }
+}
